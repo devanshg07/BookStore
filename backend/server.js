904be98241993f0bc1e40ce71f81324eb6c5a356
Devansh Goyal <devanshgoyal087@gmail.com>
2026-05-13 18:58:08 -0400

    create a post request, also remove config and node modules from git commit

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,48 @@
-import express from "express";
+import express, { request, response } from "express";
 import {PORT, MongoDB_URL} from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) =>{
     return res.status(200).send("Welcome!")
 });
 
-app.listen(PORT, ()=>{
-    console.log(PORT);
-})
+app.post('/books', async (req,res) =>{
+    try {
 
+        if(
+            !req.body.title || !req.body.author || !req.body.publishYear
+        ){
+            return res.status(400).send({
+                message: "missing fields"
+            })
+            }
+        const newBook = {
+            title: req.body.title,
+            author: req.body.author,
+            publishYear: req.body.publishYear,
+            };
+        
+        const book = await Book.create(newBook);
+        return res.status(201).send(book);
+        }
+        
+     catch (error) {
+        console.log(error);
+        res.status(500);
+    }
+})
 try {
     mongoose.connect(MongoDB_URL);
     console.log("mongodb integrated")
+    console.log(PORT)
 } catch (error) {
     console.log(error)    
 }
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
