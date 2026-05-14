42ba4cf6b2403ac4b24d9cf83cf09f7dfbdf3e75
Devansh Goyal <devanshgoyal087@gmail.com>
2026-05-13 22:09:53 -0400

    full crud api ready

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,74 @@ app.post('/books', async (req,res) =>{
         res.status(500);
     }
 })
+
+app.get('/books', async (req,res) =>{
+    try {
+        const books = await Book.find({});
+        return res.status(200).json({
+            count: books.length,
+            data: books,
+        });
+
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+app.get('/books/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const books = await Book.findById(id)
+
+        return res.status(200).json({
+            count: books.length,
+            data: books
+        })
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+app.put('/books/:id', async (req, res) => {
+    try {
+
+        if(
+            !req.body.title || !req.body.author || !req.body.publishYear
+        ){
+            return res.status(400).send({
+                message: "missing fields"
+            })
+            }
+   const { id } = req.params;
+    const result = await Book.findByIdAndUpdate(id, req.body);
+
+    if(!result){
+        return res.status(400).send("Book not found");
+    }
+    return res.status(200).send("Book details changed!")
+        
+    } catch (error) {
+        console.log(error);
+    }
+});
+app.delete('/books/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const result = await Book.findByIdAndDelete(id);
+
+        if (!result) {
+            return res.status(404).send("Book not found");
+        }
+
+        return res.status(200).send("Book deleted!");
+
+    } catch (error) {
+        console.log(error);
+
+        return res.status(500).send("Server error");
+    }
+});
 try {
     mongoose.connect(MongoDB_URL);
     console.log("mongodb integrated")
